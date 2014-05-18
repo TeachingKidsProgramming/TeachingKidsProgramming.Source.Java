76b8264f57eb8ddce69ccc85e328031909f984ae
Llewellyn Falco <isidore@setgame.com>
2014-05-17 23:37:18 -0700

    TreeQuiz w/ Lynn

--- src/org/teachingkidsprogramming/recipes/quizzes/tests/TreeQuizTest.java
+++ src/org/teachingkidsprogramming/recipes/quizzes/tests/TreeQuizTest.java
@@ -34,12 +34,13 @@ public class TreeQuizTest extends TestCase
     private void setNinety()
     {
       // sets the angle of the current turn to 90
-      turn = 90;
+      angles.put(turn, 90);
     }
     //Question4
     //create a sub called AngleFive which 
     private void angleFive()
     {
+      angles.put(5, 36);
     }
     // sets the 5th angle to 36 degrees
   }
@@ -49,29 +50,41 @@ public class TreeQuizTest extends TestCase
     new TreeQuizGrader().grade(new TreeCorrectQuiz());
     TortoiseUtils.verifyForOs();
   }
-  //  public static class TreeIncorrectQuiz extends TreeQuiz
-  //  {
-  //    @Override
-  //    public void question1()
-  //    {
-  //    }
-  //    @Override
-  //    public void question2()
-  //    {
-  //    }
-  //    @Override
-  //    public void question3()
-  //    {
-  //    }
-  //    @Override
-  //    public void question4()
-  //    {
-  //    }
-  //  }
-  //  public void testIncorrect() throws Exception
-  //  {
-  //    TreeQuizGrader.TURTLE_SPEED = Turtle.TEST_SPEED;
-  //    new TreeQuizGrader().grade(new TreeIncorrectQuiz());
-  //    TortoiseUtils.verifyForOs();
-  //  }
+  public static class TreeIncorrectQuiz extends TreeQuizAdapter
+  {
+    //  Sub question1
+    private void doubleLength()
+    {
+      // make the current length twice what it is
+      length = length + 2;
+    }
+    //      EndSub
+    //Question2
+    // create a sub called DecreaseTurn which 
+    private void decreaseTurn()
+    {
+      turn = turn + 1;
+    }
+    // decrease the current turn by 1
+    //Question3
+    //create a sub called SetNinety which 
+    private void setNinety()
+    {
+      // sets the angle of the current turn to 90
+      angles.put(turn, 30);
+    }
+    //Question4
+    //create a sub called AngleFive which 
+    private void angleFive()
+    {
+      angles.put(36, 5);
+    }
+    // sets the 5th angle to 36 degrees
+  }
+  public void testIncorrect() throws Exception
+  {
+    TreeQuizGrader.TURTLE_SPEED = Turtle.TEST_SPEED;
+    new TreeQuizGrader().grade(new TreeIncorrectQuiz());
+    TortoiseUtils.verifyForOs();
+  }
 }
--- src/org/teachingkidsprogramming/section05recursion/TreeQuiz.java
+++ src/org/teachingkidsprogramming/section05recursion/TreeQuiz.java
@@ -1,23 +1,38 @@
-package org.teachingkidsprogramming.recipes.quizzes;
+package org.teachingkidsprogramming.section05recursion;
 
-public class TreeQuiz
+import org.teachingkidsprogramming.recipes.quizzes.graders.TreeQuizAdapter;
+import org.teachingkidsprogramming.recipes.quizzes.graders.TreeQuizGrader;
+
+public class TreeQuiz extends TreeQuizAdapter
 {
-  //  Quiz.StartTreeQuizAt = question1
-  //variables for the quiz
-  //      angles[1] = 90
-  //      length = 1
-  //      turn = 1
-  //--------------------Begin Quiz --------------------
-  //      Sub question1
-  // make the current length twice what it is
-  //      EndSub
-  //Question2
-  // create a sub called DecreaseTurn which 
-  // decrease the current turn by 1
-  //Question3
-  //create a sub called SetNinety which 
-  // sets the angle of the current turn to 90
-  //Question4
-  //create a sub called AngleFive which 
-  // sets the 5th angle to 36 degrees
+  public void questions1To4()
+  {
+    //   Question 1 
+    //   doubleLength (recipe below) 
+    //   ------------- Recipe for doubleLength 
+    //       make the current length twice what it is
+    //   ------------- End of doubleLength recipe
+    //
+    //   Question 2 
+    //   decreaseTurn (recipe below) 
+    //   ------------- Recipe for decreaseTurn 
+    //      decrease the current turn by 1
+    //   ------------- End of decreaseTurn recipe
+    //
+    //   Question 3 
+    //   setNinety (recipe below) 
+    //   ------------- Recipe for setNinety 
+    //       sets the angle of the current turn to 90
+    //   ------------- End of setNinety recipe
+    //
+    //   Question 4 
+    //   angleFive (recipe below) 
+    //   ------------- Recipe for angleFive 
+    //       sets the 5th angle to 36 degrees
+    //   ------------- End of angleFive recipe
+  }
+  public static void main(String[] args)
+  {
+    new TreeQuizGrader().grade(new TreeQuiz());
+  }
 }
