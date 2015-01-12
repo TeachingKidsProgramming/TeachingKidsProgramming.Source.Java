0653fd631b4dcedb729dd81c03696c219591e26b
Lynn Langit <lynnlangit@live.com>
2015-01-11 21:05:12 -0800

    Added Oval

---
+++ src/org/teachingextensions/logo/shapes/Oval.java
@@ -0,0 +1,46 @@
+package org.teachingextensions.logo.shapes;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+import javax.swing.JPanel;
+
+import org.teachingextensions.logo.Colors;
+import org.teachingextensions.logo.Paintable;
+import org.teachingextensions.windows.ProgramWindow;
+
+public class Oval implements Paintable
+{
+  private final int   radius;
+  private final Color mainColor;
+  private int         x;
+  private int         y;
+  private int         percentTransparent;
+  public Oval(int radius, Color color)
+  {
+    this.radius = radius;
+    this.mainColor = color;
+  }
+  public void setCenter(int x, int y)
+  {
+    this.x = x;
+    this.y = y;
+  }
+  public void addTo(ProgramWindow panel)
+  {
+    panel.addPaintable(this);
+  }
+  @Override
+  public void paint(Graphics2D g, JPanel caller)
+  {
+    Color color2 = Colors.getTransparentVersion(mainColor, percentTransparent);
+    g.setColor(color2);
+    //need to incorporate radius, x, y
+    g.drawOval(x + 320, y - 20, radius + 250, radius + 140);
+    g.fillOval(75, 140, 250, 400);
+  }
+  public void setTransparency(int percentTransparent)
+  {
+    this.percentTransparent = percentTransparent;
+  }
+}
