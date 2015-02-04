3511b083a175072fa85f6d20968b4b923545a9fa
Lynn Langit <lynnlangit@live.com>
2015-02-04 13:35:48 -0800

    Updated FizzBuzz - Koan and TDD

--- src/org/teachingkidsprogramming/recipes/completed/FizzBuzzTDD.java
+++ src/org/teachingkidsprogramming/recipes/completed/FizzBuzzTDD.java
@@ -1,7 +1,14 @@
-package org.teachingkidsprogramming.section08tdd;
+package org.teachingkidsprogramming.recipes.completed;
 
 public class FizzBuzzTDD
 {
+  public static String convert(int i)
+  {
+    if (0 == i % 15) { return "FizzBuzz"; }
+    if (0 == i % 5) { return "Buzz"; }
+    if (0 == i % 3) { return "Fizz"; }
+    return "" + i;
+  }
   //for the numbers being tested, print out either that number, or, 
   //if that number is evenly divisible by 3, then print the word 'Fizz', 
   //if that number is evenly divisible by 5, then print the word 'Buzz', 
---
+++ src/org/teachingkidsprogramming/recipes/completed/FizzBuzzTDDTest.java
@@ -0,0 +1,51 @@
+package org.teachingkidsprogramming.recipes.completed;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class FizzBuzzTDDTest
+{
+  @Test
+  public void test1Returns1()
+  {
+    String result = FizzBuzzTDD.convert(1);
+    assertEquals("1", result);
+  }
+  @Test
+  public void test2Returns2()
+  {
+    String result = FizzBuzzTDD.convert(2);
+    assertEquals("2", result);
+  }
+  @Test
+  public void test3ReturnsFizz()
+  {
+    String result = FizzBuzzTDD.convert(3);
+    assertEquals("Fizz", result);
+  }
+  @Test
+  public void test5ReturnsBuzz()
+  {
+    String result = FizzBuzzTDD.convert(5);
+    assertEquals("Buzz", result);
+  }
+  @Test
+  public void test6ReturnsFizz()
+  {
+    String result = FizzBuzzTDD.convert(6);
+    assertEquals("Fizz", result);
+  }
+  @Test
+  public void test10ReturnsBuzz()
+  {
+    String result = FizzBuzzTDD.convert(10);
+    assertEquals("Buzz", result);
+  }
+  @Test
+  public void test15ReturnsFizzBuzz()
+  {
+    String result = FizzBuzzTDD.convert(15);
+    assertEquals("FizzBuzz", result);
+  }
+}
