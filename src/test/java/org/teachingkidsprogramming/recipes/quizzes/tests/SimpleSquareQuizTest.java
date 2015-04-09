4ceba4cee701f930874ad940d0e2da164d86c42c
Jim Counts <fritzjob18@nethere.com>
2015-04-09 13:05:40 +0000

    More headless

--- src/test/java/org/teachingkidsprogramming/recipes/quizzes/tests/SimpleSquareQuizTest.java
+++ src/test/java/org/teachingkidsprogramming/recipes/quizzes/tests/SimpleSquareQuizTest.java
@@ -2,6 +2,8 @@ package org.teachingkidsprogramming.recipes.quizzes.tests;
 
 import junit.framework.TestCase;
 
+import org.junit.Test;
+import org.teachingextensions.approvals.lite.util.JUnitUtils;
 import org.teachingextensions.approvals.lite.reporters.ClipboardReporter;
 import org.teachingextensions.approvals.lite.reporters.DiffReporter;
 import org.teachingextensions.approvals.lite.reporters.UseReporter;
@@ -12,61 +14,65 @@ import org.teachingextensions.logo.utils.TortoiseUtils;
 import org.teachingkidsprogramming.recipes.quizzes.graders.SimpleSquareQuizGrader;
 import org.teachingkidsprogramming.recipes.quizzes.graders.SquareQuiz;
 
-@UseReporter({ ClipboardReporter.class, DiffReporter.class })
-public class SimpleSquareQuizTest extends TestCase {
-	public static class SimpleSquareCorrectQuiz implements SquareQuiz {
-		public void question1() {
-			// Move the tortoise 110 pixels
-			Tortoise.move(110);
-		}
+@UseReporter({ClipboardReporter.class, DiffReporter.class})
+public class SimpleSquareQuizTest {
+    public static class SimpleSquareCorrectQuiz implements SquareQuiz {
+        public void question1() {
+            // Move the tortoise 110 pixels
+            Tortoise.move(110);
+        }
 
-		public void question2() {
-			// Turn the tortoise 1/5 of 360 degrees to the right
-			Tortoise.turn(360 / 5);
-		}
+        public void question2() {
+            // Turn the tortoise 1/5 of 360 degrees to the right
+            Tortoise.turn(360 / 5);
+        }
 
-		public void question3() {
-			// Change the color the tortoise draws to yellow
-			Tortoise.setPenColor(PenColors.Yellows.Yellow);
-		}
+        public void question3() {
+            // Change the color the tortoise draws to yellow
+            Tortoise.setPenColor(PenColors.Yellows.Yellow);
+        }
 
-		public void question4() {
-			// Change the width of the line the tortoise draws to 5 pixels
-			Tortoise.setPenWidth(5);
-		}
-	}
+        public void question4() {
+            // Change the width of the line the tortoise draws to 5 pixels
+            Tortoise.setPenWidth(5);
+        }
+    }
 
-	public void testCorrect() throws Exception {
-		SimpleSquareQuizGrader.TURTLE_SPEED = Turtle.TEST_SPEED;
-		new SimpleSquareQuizGrader().grade(new SimpleSquareCorrectQuiz());
-		TortoiseUtils.verifyForOs();
-	}
+    @Test
+    public void testCorrect() throws Exception {
+        JUnitUtils.assumeNotHeadless();
+        SimpleSquareQuizGrader.TURTLE_SPEED = Turtle.TEST_SPEED;
+        new SimpleSquareQuizGrader().grade(new SimpleSquareCorrectQuiz());
+        TortoiseUtils.verifyForOs();
+    }
 
-	public static class SimpleSquareIncorrectQuiz implements SquareQuiz {
-		public void question1() {
-			// Move the tortoise 110 pixels
-			Tortoise.move(75);
-		}
+    public static class SimpleSquareIncorrectQuiz implements SquareQuiz {
+        public void question1() {
+            // Move the tortoise 110 pixels
+            Tortoise.move(75);
+        }
 
-		public void question2() {
-			// Turn the tortoise 1/5 of 360 degrees to the right
-			Tortoise.turn(360.0 / 6);
-		}
+        public void question2() {
+            // Turn the tortoise 1/5 of 360 degrees to the right
+            Tortoise.turn(360.0 / 6);
+        }
 
-		public void question3() {
-			// Change the color the tortoise draws to yellow
-			Tortoise.setPenColor(PenColors.Yellows.Gold);
-		}
+        public void question3() {
+            // Change the color the tortoise draws to yellow
+            Tortoise.setPenColor(PenColors.Yellows.Gold);
+        }
 
-		public void question4() {
-			// Change the width of the line the tortoise draws to 5 pixels
-			Tortoise.setPenWidth(9);
-		}
-	}
+        public void question4() {
+            // Change the width of the line the tortoise draws to 5 pixels
+            Tortoise.setPenWidth(9);
+        }
+    }
 
-	public void testIncorrect() throws Exception {
-		SimpleSquareQuizGrader.TURTLE_SPEED = Turtle.TEST_SPEED;
-		new SimpleSquareQuizGrader().grade(new SimpleSquareIncorrectQuiz());
-		TortoiseUtils.verifyForOs();
-	}
+    @Test
+    public void testIncorrect() throws Exception {
+        JUnitUtils.assumeNotHeadless();
+        SimpleSquareQuizGrader.TURTLE_SPEED = Turtle.TEST_SPEED;
+        new SimpleSquareQuizGrader().grade(new SimpleSquareIncorrectQuiz());
+        TortoiseUtils.verifyForOs();
+    }
 }
