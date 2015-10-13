456097f1f482674221f3fbc3234acaaf9f22f2e7
lynnlangit <lynnlangit@live.com>
2015-10-12 18:32:15 -0700

    Updated Spiral to surface runtime exception

--- src/main/java/org/teachingkidsprogramming/recipes/completed/section01forloops/Spiral.java
+++ src/main/java/org/teachingkidsprogramming/recipes/completed/section01forloops/Spiral.java
@@ -3,6 +3,7 @@ package org.teachingkidsprogramming.recipes.completed.section01forloops;
 import org.teachingextensions.logo.ColorWheel;
 import org.teachingextensions.logo.PenColors;
 import org.teachingextensions.logo.Tortoise;
+import org.teachingextensions.windows.MessageBox;
 
 public class Spiral
 {
@@ -21,12 +22,19 @@ public class Spiral
     //    Do the following 75 times --#3
     for (int i = 0; i < 75; i++)
     {
-      //         Change the color of the line the tortoise draws the next color on the Color Wheel --#6
-      Tortoise.setPenColor(ColorWheel.getNextColor());
-      //         Move the tortoise 5 times the current line number you are drawing --#5
-      Tortoise.move(i * 5);
-      //         Turn the tortoise 1/3 of 360 degrees to the right --#2
-      Tortoise.turn(360.0 / 3);
+      try
+      {
+        //         Change the color of the line the tortoise draws the next color on the Color Wheel --#6
+        Tortoise.setPenColor(ColorWheel.getNextColor());
+        //         Move the tortoise 5 times the current line number you are drawing --#5
+        Tortoise.move(i * 5);
+        //         Turn the tortoise 1/3 of 360 degrees to the right --#2
+        Tortoise.turn(360.0 / 3);
+      }
+      catch (RuntimeException re)
+      {
+        MessageBox.showMessage("Hold up: " + re);
+      }
     }
   }
 }
