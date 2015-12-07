ae478070c41c67a36d56e95b318e8f50a9ae1faa
lynnlangit <lynnlangit@live.com>
2015-12-06 22:05:36 -0800

    added to OneFishTwoFish

--- src/main/java/org/teachingkidsprogramming/recipes/completed/section06modelviewcontroller/OneFishTwoFish.java
+++ src/main/java/org/teachingkidsprogramming/recipes/completed/section06modelviewcontroller/OneFishTwoFish.java
@@ -10,6 +10,7 @@ import org.teachingextensions.logo.utils.EventUtils.FancyMessageBox;
 //*************This Lesson is In Development*****************************//
 public class OneFishTwoFish
 {
+  // Create a Scanner to make a string
   private static Scanner scanner;
   public static void main(String[] args)
   {
@@ -20,22 +21,32 @@ public class OneFishTwoFish
   public static void makeAString()
   {
     final String input = "1 fish 2 fish red fish blue fish,black fish,blue fish,old fish,new fish ";
+    // Use your scanner with your input
     scanner = new Scanner(input);
     System.err.println("\nWe have:  " + input + '\n');
+    // Now, tell a story with a new Scanner instance
     tellAStory(input);
   }
   private static void tellAStory(final String input)
   {
+    // Use 'fish' as the base text w/your scanner [story]
     Scanner s = scanner.useDelimiter("\\s*fish\\s*");
+    // Get the next number to use in your scanner [story]
     System.out.println("So:       " + s.nextInt() + " and " + s.nextInt());
+    // Get the next value to use in your scanner [story]
     System.out.println("And:      " + s.next() + " and " + s.next() + '\n');
+    // Iterate over each fish [string]
     for (String fish : new Iterable<String>()
     {
       @Override
+      // Create a new string iterator 
       public Iterator<String> iterator()
       {
+        // Create a new scanner for your input
         scanner = new Scanner(input);
+        // Use a comma to separate the strings in your story
         scanner.useDelimiter(",");
+        // Return the result
         return scanner;
       }
     })
@@ -46,24 +57,36 @@ public class OneFishTwoFish
   }
   public static void makeAFishyDecision(int numberOfFish)
   {
-    String image = "//Users//lynnlangit//Documents//workspace//TeachingKidsProgramming.Source.Java//src//main//resources//icons//thumb-up.png";
-    final ImageIcon icon = new ImageIcon(image);
+    // Use a switch...case on the number of fish
     switch (numberOfFish)
     {
+      // If number of fish is -1
       case -1 :
+        String image = "//Users//lynnlangit//Documents//workspace//TeachingKidsProgramming.Source.Java//src//main//resources//icons//thumb-up.png";
+        ImageIcon icon = new ImageIcon(image);
+        // Show a message with the fancy message box this text, this title and this icon...
         FancyMessageBox.showMesage("Had a Fish", "Not hungry anymore...", icon);
+        // End
         break;
       case 0 :
-        FancyMessageBox.showMesage("No Fish", "None", icon);
+        String image0 = "//Users//lynnlangit//Documents//workspace//TeachingKidsProgramming.Source.Java//src//main//resources//icons//information.png";
+        ImageIcon icon0 = new ImageIcon(image0);
+        FancyMessageBox.showMesage("No Fish", "Still hungry", icon0);
         break;
       case 1 :
-        FancyMessageBox.showMesage("One Fish", "One", icon);
+        String image1 = "//Users//lynnlangit//Documents//workspace//TeachingKidsProgramming.Source.Java//src//main//resources//icons//star.png";
+        ImageIcon icon1 = new ImageIcon(image1);
+        FancyMessageBox.showMesage("One Fish", "This one has a little star", icon1);
         break;
       case 2 :
-        FancyMessageBox.showMesage("Two Fish", "Two", icon);
+        String image2 = "//Users//lynnlangit//Documents//workspace//TeachingKidsProgramming.Source.Java//src//main//resources//icons//github.png";
+        ImageIcon icon2 = new ImageIcon(image2);
+        FancyMessageBox.showMesage("Two Fish", "Funny things are everywhere", icon2);
         break;
       default :
-        FancyMessageBox.showMesage("Vegetaraian meal", "Fish are icky", icon);
+        String image4 = "//Users//lynnlangit//Documents//workspace//TeachingKidsProgramming.Source.Java//src//main//resources//icons//hint.png";
+        ImageIcon icon4 = new ImageIcon(image4);
+        FancyMessageBox.showMesage("Vegetaraian meal", "Fish are icky", icon4);
         break;
     }
   }
