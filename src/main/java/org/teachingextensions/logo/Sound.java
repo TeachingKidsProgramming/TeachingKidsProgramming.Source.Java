9cd447322420e167e2c1e5b98e4b9bd9006b1d9c
lynnlangit <lynnlangit@live.com>
2016-01-03 21:13:03 -0800

    Added 'Love Being a Turtle' Sound w/ @jamesrcounts

--- src/main/java/org/teachingextensions/logo/Sound.java
+++ src/main/java/org/teachingextensions/logo/Sound.java
@@ -1,28 +1,27 @@
 package org.teachingextensions.logo;
 
+import java.awt.Toolkit;
+import java.net.URL;
+
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-import java.awt.*;
-import java.net.URL;
 
 /**
  * <img src="http://www.spellzone.com/images/sound-icon.gif" style="text-align: left" alt="A speaker with sound waves" >
  * Sound allows you to play a sound, like a 'beep' or the sound from a file
  */
 public class Sound
-
 {
   private URL soundUrl = null;
-
-  public Sound() {
+  public Sound()
+  {
     this(TKPSound.Yay);
   }
-
-  public Sound(TKPSound sound) {
+  public Sound(TKPSound sound)
+  {
     this.setSound(sound);
   }
-
   /**
    * Plays a beep through your speakers. BEEP!<br>
    * <b>Example:</b> {@code  Sound.playBeep()}
@@ -31,7 +30,6 @@ public class Sound
   {
     Toolkit.getDefaultToolkit().beep();
   }
-
   /**
    * Sets a sound that you can play through your speakers.
    * Use a TKPSound (there is a list)<br>
@@ -48,7 +46,6 @@ public class Sound
     if (resource == null) { throw new IllegalStateException("Could not get TKPSound: " + sound); }
     this.soundUrl = resource;
   }
-
   /**
    * Plays a TKPSound through your speakers.
    * You must first set the TKPSound <br>
@@ -80,7 +77,8 @@ public class Sound
    * Check out our cool sounds - thanks to http://www.wavsource.com/
    */
   public enum TKPSound {
-    Ahem, Applause, BrainIsGone, CatMeow, Cheering, Crickets, DoctorMccoy, Fanfare, FargoYah, Gong, LLCoolJYaKnow,
-    LosingPower, Malfunction, Pizza, RunAway, SheerIgnorance, SoBeIt, StrangePerson, Stubborn, Yahoo, Yay
+                        Ahem, Applause, BrainIsGone, CatMeow, Cheering, Crickets, DoctorMccoy, Fanfare, FargoYah,
+                        Gong, LLCoolJYaKnow, LosingPower, LoveBeingATurtle, Malfunction, Pizza, RunAway,
+                        SheerIgnorance, SoBeIt, StrangePerson, Stubborn, Yahoo, Yay
   }
 }
