34c8d450ee948831e306858a85e4fc3c1e275666
lynnlangit <lynnlangit@live.com>
2016-02-16 21:35:15 -0800

    completed FizzBuzzGoldenMaster w/@samanthalangit

--- src/main/java/org/teachingkidsprogramming/recipes/completed/section09final/FizzBuzzGoldenMaster.java
+++ src/main/java/org/teachingkidsprogramming/recipes/completed/section09final/FizzBuzzGoldenMaster.java
@@ -1,37 +1,32 @@
 package org.teachingkidsprogramming.recipes.completed.section09final;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.teachingextensions.approvals.lite.Approvals;
 
-//********In Progress************************//
 public class FizzBuzzGoldenMaster
 {
-  //   Remove this when working - for build process only
-  @Ignore
   @Test
   public void testNumbers() throws Exception
   {
-    //  Arrange - create a list of numbers from 1-20
-    Integer[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
-    //  Act - call the convertNumbers method on the list
-    //  Assert that the list is a Golden Master
-    Approvals.verify(numbers);
-  }
-  public static String convertNumbers(Integer[] numbers)
-  {
-    for (Integer number : numbers)
+    //  Arrange - create a list of numbers from 1-100
+    StringBuilder sb = new StringBuilder();
+    for (int i = 1; i < 101; i++)
     {
-      convert(number);
+      //  Act - call the convertNumbers method on the list
+      sb.append(convert(i));
     }
-    //need to fix or override the ToString here
-    return ("" + numbers);
+    //  IMPORTANT - This testing method uses a file compare tool
+    //  You could use 'BeyondCompare' for this
+    //  Download from -- http://www.scootersoftware.com/download.php
+    //
+    //  Assert that the list is a Golden Master (uses .received and .approved files)
+    Approvals.verify(sb);
   }
   public static String convert(int i)
   {
-    if (0 == i % 15) { return "\n FizzBuzz"; }
-    if (0 == i % 5) { return "\n Buzz"; }
-    if (0 == i % 3) { return "\n Fizz"; }
-    return "" + i;
+    if (0 == i % 15) { return "\nFizzBuzz"; }
+    if (0 == i % 5) { return "\nBuzz"; }
+    if (0 == i % 3) { return "\nFizz"; }
+    return "\n" + i;
   }
 }
