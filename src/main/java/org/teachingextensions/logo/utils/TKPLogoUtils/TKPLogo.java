81d882f8a6692e9330cc629877547dc6376804d1
lynnlangit <lynnlangit@live.com>
2016-03-06 13:48:58 -0800

    small refactoring for new recipe TKPLogo w/@samanthalangit

--- src/main/java/org/teachingextensions/logo/utils/TKPLogoUtils/TKPLogo.java
+++ src/main/java/org/teachingextensions/logo/utils/TKPLogoUtils/TKPLogo.java
@@ -6,13 +6,13 @@ import org.teachingextensions.logo.utils.ColorUtils.PenColors;
 import org.teachingkidsprogramming.recipes.completed.section01forloops.MakeALogo;
 
 /**
- * <img src="https://avatars0.githubusercontent.com/u/4691010?v=3&s=200" style="text-align: left" alt="The TKP logo" height="50"><br/>
- * The TKPLogo allows you to create a TKP logo 
+ * <img src="https://avatars0.githubusercontent.com/u/4691010?v=3&s=200" style="text-align: left" alt="The TKP logo" height="40">
+ * The TKPLogo allows <b>you</b> to create a super cool TKP logo on your window
  */
 public class TKPLogo
 {
   /**
-   * Sets up the Colors on the Color Wheel
+   * Sets up a particular set of Colors on the Color Wheel
    * <div><b>Example:</b> {@code TKPLogo.setupColorWheel();}</div>
    */
   public static void setupColorWheel()
@@ -27,11 +27,15 @@ public class TKPLogo
     ColorWheel.addColor(PenColors.Oranges.Orange);
     ColorWheel.addColor(PenColors.Reds.Red);
     ColorWheel.addColor(PenColors.Whites.White);
+    setupBackgroundAndLines();
+  }
+  private static void setupBackgroundAndLines()
+  {
     Tortoise.getBackgroundWindow().setBackground(ColorWheel.getRandomColorFromWheel());
     Tortoise.setPenWidth(4);
   }
   /**
-   * Draws an TKP inner P
+   * Draws an <i>amazing</> TKP inner P
    * <div><b>Example:</b> {@code TKPLogo.draw_inner_tkp_P();}</div>
    */
   public static void draw_inner_tkp_P()
@@ -48,7 +52,7 @@ public class TKPLogo
     Tortoise.turn(1);
   }
   /**
-   * Draws an TKP outer P
+   * Draws an <i>awesome</> TKP outer P
    * <div><b>Example:</b> {@code TKPLogo.draw_outer_tkp_P();}</div>
    */
   public static void draw_outer_tkp_P()
@@ -80,7 +84,7 @@ public class TKPLogo
     Tortoise.move(43);
   }
   /**
-   * Draws an TKP right bracket
+   * Draws an <i>outstanding</> TKP right bracket
    * <div><b>Example:</b> {@code TKPLogo.drawRightBracket();}</div>
    */
   public static void drawRightBracket()
@@ -118,7 +122,7 @@ public class TKPLogo
     Tortoise.move(11);
   }
   /**
-   * Draws an TKP left bracket
+   * Draws an <i>original</> TKP left bracket
    * <div><b>Example:</b> {@code TKPLogo.drawLeftBracket();}</div>
    */
   public static void drawLeftBracket()
@@ -155,7 +159,7 @@ public class TKPLogo
     Tortoise.move(10);
   }
   /**
-   * Draws an TKP T
+   * Draws an <i>incredible</> TKP T
    * <div><b>Example:</b> {@code TKPLogo.draw_tkp_T();}</div>
    */
   public static void draw_tkp_T()
@@ -193,6 +197,39 @@ public class TKPLogo
     Tortoise.move(15);
     Tortoise.turn(90);
     Tortoise.move(50);
+  }
+  /**
+   * Draws an <i>impressive</> TKP K
+   * <div><b>Example:</b> {@code TKPLogo.draw_tkp_K();}</div>
+   */
+  public static void draw_tkp_K()
+  {
+    Tortoise.setAngle(180);
+    Tortoise.setX(MakeALogo.XValue + 150);
+    Tortoise.setY(MakeALogo.YValue + 15);
+    Tortoise.turn(180);
+    Tortoise.move(200);
+    Tortoise.turn(90);
+    Tortoise.move(30);
+    Tortoise.turn(90);
+    Tortoise.move(128);
+    Tortoise.turn(-150);
+    Tortoise.move(70);
+    Tortoise.turn(90);
+    Tortoise.move(30);
+    Tortoise.turn(90);
+    Tortoise.move(70);
+    Tortoise.turn(-80);
+    Tortoise.move(80);
+    Tortoise.turn(140);
+    Tortoise.move(40);
+    Tortoise.turn(40);
+    Tortoise.move(60);
+    Tortoise.turn(-130);
+    Tortoise.move(45);
+    Tortoise.turn(90);
+    Tortoise.move(30);
+    Tortoise.turn(2);
   }
   private static void curve6()
   {
@@ -249,38 +286,5 @@ public class TKPLogo
       Tortoise.turn(-1);
       Tortoise.move(size);
     }
-  }
-  /**
-   * Draws an TKP K
-   * <div><b>Example:</b> {@code TKPLogo.draw_tkp_K();}</div>
-   */
-  public static void draw_tkp_K()
-  {
-    Tortoise.setAngle(180);
-    Tortoise.setX(MakeALogo.XValue + 150);
-    Tortoise.setY(MakeALogo.YValue + 15);
-    Tortoise.turn(180);
-    Tortoise.move(200);
-    Tortoise.turn(90);
-    Tortoise.move(30);
-    Tortoise.turn(90);
-    Tortoise.move(128);
-    Tortoise.turn(-150);
-    Tortoise.move(70);
-    Tortoise.turn(90);
-    Tortoise.move(30);
-    Tortoise.turn(90);
-    Tortoise.move(70);
-    Tortoise.turn(-80);
-    Tortoise.move(80);
-    Tortoise.turn(140);
-    Tortoise.move(40);
-    Tortoise.turn(40);
-    Tortoise.move(60);
-    Tortoise.turn(-130);
-    Tortoise.move(45);
-    Tortoise.turn(90);
-    Tortoise.move(30);
-    Tortoise.turn(2);
   }
 }
