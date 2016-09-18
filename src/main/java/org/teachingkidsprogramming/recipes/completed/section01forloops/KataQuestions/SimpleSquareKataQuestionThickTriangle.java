5cfb054e15d393087e20034bb558494c6d67e0d8
lynnlangit <lynnlangit@live.com>
2016-09-18 15:58:13 -0700

    added 2 new kata questions for course 1 w/@dcoopersmith
    added kata questions folder

--- src/main/java/org/teachingkidsprogramming/recipes/completed/section01forloops/KataQuestions/SimpleSquareKataQuestionThickTriangle.java
+++ src/main/java/org/teachingkidsprogramming/recipes/completed/section01forloops/KataQuestions/SimpleSquareKataQuestionThickTriangle.java
@@ -1,26 +1,27 @@
-package org.teachingkidsprogramming.recipes.completed.section01forloops;
+package org.teachingkidsprogramming.recipes.completed.section01forloops.KataQuestions;
 
 import org.teachingextensions.logo.Tortoise;
 import org.teachingextensions.logo.utils.ColorUtils.PenColors;
 
 //
 //------------Kata Question---------------//
-//  How would you make a triangle?
+//  How would you make a triangle with 20px thick sides?
 //  Write out the steps in English 
 //  Then translate the steps into code
 //  Make sure to run after each line
 //
-public class SimpleSquareKataQuestion
+public class SimpleSquareKataQuestionThickTriangle
 {
   public static void main(String[] args) throws Exception
   {
     Tortoise.show();
     Tortoise.setSpeed(10);
-    for (int i = 0; i < 4; i++)
+    int sides = 3;
+    for (int i = 0; i < sides; i++)
     {
       Tortoise.setPenColor(PenColors.Blues.Blue);
       Tortoise.move(50);
-      Tortoise.turn(90);
+      Tortoise.turn(360 / sides);
     }
   }
 }
