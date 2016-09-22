5b450cfd9827fda204a7ca7bb1f230e9327d79fc
lynnlangit <lynnlangit@live.com>
2016-09-21 19:21:55 -0700

    new full square kata w/@dcoopersmith

---
+++ src/main/java/org/teachingkidsprogramming/recipes/completed/section01forloops/KataQuestions/CompleteSquare.java
@@ -0,0 +1,15 @@
+package org.teachingkidsprogramming.recipes.completed.section01forloops.KataQuestions;
+
+//------------Square Kata---------------//
+//  Use the Tortoise to draw a red square with a width and height of 40 pixels
+//  Write each of the English line comments (use at least 6 line comments)
+//  Number each comment line at the end, so your user knows the correct order to translate the code
+//  Verify - step one -  Translate EACH comment line into code 
+//  Verify - step two -  Run your code after each line
+public class CompleteSquare
+{
+  public static void main(String[] args) throws Exception
+  {
+    //TODO: write comments and then code here
+  }
+}
