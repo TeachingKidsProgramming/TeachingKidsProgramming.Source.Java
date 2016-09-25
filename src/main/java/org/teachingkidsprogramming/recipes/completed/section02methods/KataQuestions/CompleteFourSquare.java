55e078fb784d1c3bc6dd4420f834713107be620d
lynnlangit <lynnlangit@live.com>
2016-09-25 13:44:39 -0700

    added FourSquareKata and Snowflake Kata

---
+++ src/main/java/org/teachingkidsprogramming/recipes/completed/section02methods/KataQuestions/CompleteFourSquare.java
@@ -0,0 +1,16 @@
+package org.teachingkidsprogramming.recipes.completed.section02methods.KataQuestions;
+
+//------------FourSquare Kata---------------//
+//  Use the Tortoise to draw four squares, each with a width and height of 50 pixels
+//  Write each of the English line comments (use at least 8 line comments)
+//  Number each comment line at the end
+//  Verify - step one -  Translate EACH comment line into code 
+//  Verify - step two -  Run your code after each line
+public class CompleteFourSquare
+{
+  public static void main()
+  {
+    System.out.println("be sure to complete the main method to start");
+    //TODO: write comments and then code here
+  }
+}
